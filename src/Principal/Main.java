2a1b62c79548fbfe11d5803bb3857074852a9305
Raul_LA <rleonalvarez090@gmail.com>
2023-09-13 01:38:02 -0500

    archivo principal (+)

---
+++ src/Principal/Main.java
@@ -0,0 +1,12 @@
+
+package Principal;
+import DAO.*;
+
+public class Main {
+    
+    static ConexionBD cBD;
+    public static void main(String[] args) {
+        cBD = new ConexionBD();
+    }
+    
+}
