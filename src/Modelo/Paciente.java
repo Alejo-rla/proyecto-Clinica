f5c27d516cbc60b1fecfae2629622033ab29e4b3
Raul_LA <rleonalvarez090@gmail.com>
2023-09-14 03:04:14 -0500

    class Paciente

---
+++ src/Modelo/Paciente.java
@@ -0,0 +1,40 @@
+package Modelo;
+
+
+public class Paciente {
+    private int codPaciente;
+    private String nombre;
+    private String apellido;
+    
+
+    public Paciente() {
+    }
+
+    public int getCodPaciente() {
+        return codPaciente;
+    }
+
+    public void setCodPaciente(int codPaciente) {
+        this.codPaciente = codPaciente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    
+    
+    
+}
